35ce27f55d6a270efd4f000c4f41a96998a13420
MoonKabir <moonkabir4@gmail.com>
2021-06-21 07:56:48 +0600

    tic tac toe project start and 9.2/62

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Game from './components/game';
+
+const App = () =>{
+    return (
+        <div className="App">
+            <h1>Tic Tac Toe</h1>
+            <Game/>
+        </div>
+    )
+}
+export default App
